d11eeeec7ec3dad25680170cd307eb7e1488fb03
Mikaela Marie Cashman <mcashman2@unl.edu>
2016-08-15 12:56:11 -0400

    install script

---
+++ kbaseSetUp.sh
@@ -0,0 +1,21 @@
+#! /bin/bash 
+
+#Install docker
+curl https://get.docker.com/ | sh
+
+#Install jdk 7 (could put this into say installjdk7.sh using nano …)
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:webupd8team/java
+apt-get update
+apt-get install -y oracle-java7-installer
+rm -rf /var/cache/oracle-jdk7-installer
+
+#install ant
+apt-get install -y ant make
+
+#get jars from kbase
+git clone https://github.com/kbase/jars
+
+#get kb_sdk repo
+git clone https://github.com/kbase/kb_sdk
